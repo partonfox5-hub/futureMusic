292952b1e5bb696894fff87764f1281e73e0edc7
Partonfox5 <partonfox5@gmail.com>
2026-08-22 12:20:02 -0400

    Test SHARK: liberal sub pickup collision for torpedoes, fuel, and O2.

--- server.js
+++ server.js
@@ -583,6 +583,11 @@ app.get('/test-c4h9n2x8', (req, res) => {
     res.setHeader('X-Robots-Tag', 'noindex, nofollow');
     res.render('test-c4h9n2x8');
 });
+app.get('/test-m8q2n5k7', (req, res) => {
+    res.setHeader('X-Robots-Tag', 'noindex, nofollow');
+    res.setHeader('Permissions-Policy', 'xr-spatial-tracking=(self), fullscreen=(self), gamepad=(self), accelerometer=(self), gyroscope=(self), magnetometer=(self)');
+    res.sendFile(path.join(__dirname, 'public', 'games', 'shark-test-m8q2n5k7', 'index.html'));
+});
 
 const SHARK_YT_CHANNEL = 'UClOdltq7PUfU3cLyK0O-6jA';
 const SHARK_YT_FALLBACK = ['SQha9mRCsv8', 'hpUL9b0vVX8', 'DjZTj5Gv4NA', '01RVvhkRL_U', 'DGXsDgt5KZY', 'z1PFzPjj024', 'ZwGVGPxJO3E', 'KVKnZgM8_4g'];
